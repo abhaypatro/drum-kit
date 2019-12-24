63227dba6fd3eef83d1d3fecc5fe5a19802f868c
abhaypatro <abhaycharanpatro@gmail.com>
2019-12-24 15:24:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,93 +1,81 @@
-document.addEventListener("keydown", onKeyPressed);
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-function onKeyPressed(e) {
-  if(e.key=='w')
-  {
-    var audio = new Audio('sounds/tom-1.mp3');
-    audio.play();
-  }
-  else
-  if(e.key=='a')
-  {
-    var audio = new Audio('sounds/tom-2.mp3');
-    audio.play();
-  }
-  else
-  if(e.key=='s')
-  {
-    var audio = new Audio('sounds/tom-3.mp3');
-    audio.play();
-  }
-  else
-  if(e.key=='d')
-  {
-    var audio = new Audio('sounds/tom-4.mp3');
-    audio.play();
-  }
-  else
-  if(e.key=='j')
-  {
-    var audio = new Audio('sounds/snare.mp3');
-    audio.play();
-  }
-  else
-  if(e.key=='k')
-  {
-    var audio = new Audio('sounds/crash.mp3');
-    audio.play();
-  }
-  else
-  if(e.key=='l')
-  {
-    var audio = new Audio('sounds/kick.mp3');
-    audio.play();
-  }
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
 
 }
 
-for(var i=0;i<document.querySelectorAll("button").length;i++)
- document.querySelectorAll("button")[i].addEventListener("click", function(){
-  if(this.innerHTML=='w')
-  {
-    var audio = new Audio('sounds/tom-1.mp3');
-    audio.play();
-  }
-  else
-  if(this.innerHTML=='a')
-  {
-    var audio = new Audio('sounds/tom-2.mp3');
-    audio.play();
-  }
-  else
-  if(this.innerHTML=='s')
-  {
-    var audio = new Audio('sounds/tom-3.mp3');
-    audio.play();
-  }
-  else
-  if(this.innerHTML=='d')
-  {
-    var audio = new Audio('sounds/tom-4.mp3');
-    audio.play();
-  }
-  else
-  if(this.innerHTML=='j')
-  {
-    var audio = new Audio('sounds/snare.mp3');
-    audio.play();
-  }
-  else
-  if(this.innerHTML=='k')
-  {
-    var audio = new Audio('sounds/crash.mp3');
-    audio.play();
-  }
-  else
-  if(this.innerHTML=='l')
-  {
-    var audio = new Audio('sounds/kick.mp3');
-    audio.play();
-  }
+document.addEventListener("keypress", function(event) {
 
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
 
 });
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick.mp3');
+      kick.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
